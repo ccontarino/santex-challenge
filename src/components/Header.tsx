ab386c3918f43a45458130f4fc121152de82ce4c
Christian Contarino <ccontarino@gmail.com>
2023-06-23 20:21:42 -0300

    feat: add product card

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,37 @@
 import styled from 'styled-components';
 import { Input } from './Input/Input';
-
+// import cart from '../assets/cart-svgrepo-com.svg';
 export function Header() {
   const Header = styled.header`
     background: red;
-    min-height: 100px;
-    padding: 15px 20px;
+    padding: 20px 20px;
     box-sizing: border-box;
   `;
   const Image = styled.img`
     height: 50px;
     flex: 0.5;
   `;
-  const Profile = styled.div`
+  const Cart = styled.img`
     flex: 0.8;
+    height: 40px;
+    
+    
+  `;
+  const Box = styled.div`
+    display: flex;
   `;
 
   return (
     <Header>
-      <div style={{ display: 'flex' }}>
+      <Box>
         <Image
           src="https://santex.wpengine.com/wp-content/uploads/2019/02/logo-santex@3x.png"
           alt="logo"
         />
 
         <Input placeholder="Search.."></Input>
-        <Profile />
-      </div>
+        {/* <Cart src={cart} /> */}
+      </Box>
       {/* <div>$ 0</div> */}
     </Header>
   );
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,32 @@
+import styled from 'styled-components';
+import Product from './Product/Product';
+
 export function ProductList() {
-  return <div>List</div>;
+  const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+    grid-gap: 8px;
+  `;
+
+  return (
+    <Container>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+      <Product></Product>
+    </Container>
+  );
 }
