f4bbde600baddbf92183fdba46334728bb8fcd47
Christian Contarino <ccontarino@gmail.com>
2023-06-26 20:29:19 -0300

    test: add header snapshot and unit test

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Header } from './Header';
+import { MyContext } from '../../Context/Context';
+import { UPDATE_SLIDE_BAR } from '../../Context/constantsContext';
+
+describe('Header', () => {
+  it('should render the logo', () => {
+    const { getByAltText, ...headerComponent } = render(
+      <MyContext.Provider value={{ state: {}, dispatch: jest.fn() }}>
+        <Header />
+      </MyContext.Provider>
+    );
+    expect(headerComponent).toMatchSnapshot();
+    const logo = getByAltText('logo');
+    expect(logo).toBeInTheDocument();
+  });
+
+  it('should render the search input', () => {
+    const { getByPlaceholderText } = render(
+      <MyContext.Provider value={{ state: {}, dispatch: jest.fn() }}>
+        <Header />
+      </MyContext.Provider>
+    );
+    const searchInput = getByPlaceholderText('Search..');
+    expect(searchInput).toBeInTheDocument();
+  });
+
+  it('should render the cart icon', () => {
+    const { getByAltText } = render(
+      <MyContext.Provider value={{ state: {}, dispatch: jest.fn() }}>
+        <Header />
+      </MyContext.Provider>
+    );
+    const cartIcon = getByAltText('cart');
+    expect(cartIcon).toBeInTheDocument();
+  });
+
+  it('should open the checkout sidebar when the cart icon is clicked', () => {
+    const dispatch = jest.fn();
+    const { getByAltText } = render(
+      <MyContext.Provider
+        value={{ state: { checkoutSideBarIsOpen: false }, dispatch }}
+      >
+        <Header />
+      </MyContext.Provider>
+    );
+    const cartIcon = getByAltText('cart');
+    fireEvent.click(cartIcon);
+    expect(dispatch).toHaveBeenCalledWith({
+      type: UPDATE_SLIDE_BAR,
+      payload: true,
+    });
+  });
+});
--- src/utils/object.tsx
+++ src/utils/object.tsx
@@ -1,2 +1,8 @@
+/**
+ * Creates a deep copy of an array.
+ *
+ * @param {Array} array - The array to copy.
+ * @returns {Array} A new array that is a deep copy of the original.
+ */
 export const deepCopyArray = (array: any[]) =>
   JSON.parse(JSON.stringify(array));
